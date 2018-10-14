2e5e458a63429c869edf2c5eabdbca4f59d70a0f
Tulir Asokan <tulir@maunium.net>
2018-10-14 22:09:55 +0300

    Rewrite in Python

---
+++ jesaribot/__init__.py
@@ -0,0 +1,38 @@
+from maubot import Plugin, CommandSpec, PassiveCommand, Event
+from mautrix.types import (MessageType, MediaMessageEventContent, ImageInfo, ThumbnailInfo,
+                           ContentURI)
+
+COMMAND_JESARI = "xyz.maubot.jesari"
+
+
+class JesariBot(Plugin):
+    async def start(self) -> None:
+        self.client.set_command_spec(CommandSpec(
+            passive_commands=[PassiveCommand(
+                name=COMMAND_JESARI,
+                matches="jesari",
+                match_against="body",
+            )]
+        ))
+        self.client.add_command_handler(COMMAND_JESARI, self.handler)
+
+    @staticmethod
+    async def handler(evt: Event) -> None:
+        await evt.respond(MediaMessageEventContent(
+            msgtype=MessageType.IMAGE,
+            body="putkiteippi.gif",
+            url=ContentURI("mxc://maunium.net/IkSoSYYrtaYJQeCaABSLqKiD"),
+            info=ImageInfo(
+                mimetype="image/gif",
+                height=153,
+                width=364,
+                size=2079294,
+                thumbnail_url=ContentURI("mxc://maunium.net/iivOnCDjcGqGvnwnNWxSbAvb"),
+                thumbnail_info=ThumbnailInfo(
+                    mimetype="image/png",
+                    height=153,
+                    width=364,
+                    size=51302,
+                ),
+            ),
+        ))
