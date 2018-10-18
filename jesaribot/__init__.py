0ef1714b9d559bffff565eda860936a19f3307f9
Tulir Asokan <tulir@maunium.net>
2018-10-18 15:24:32 +0300

    Add more quality levels

--- jesaribot/__init__.py
+++ jesaribot/__init__.py
@@ -4,6 +4,81 @@ from mautrix.types import (MessageType, MediaMessageEventContent, ImageInfo, Thu
 
 COMMAND_JESARI = "xyz.maubot.jesari"
 
+quality = {
+    "crap": MediaMessageEventContent(
+        msgtype=MessageType.IMAGE,
+        body="putkiteippi.gif",
+        url=ContentURI("mxc://maunium.net/IkSoSYYrtaYJQeCaABSLqKiD"),
+        info=ImageInfo(
+            mimetype="image/gif",
+            width=364,
+            height=153,
+            size=2079294,
+            thumbnail_url=ContentURI("mxc://maunium.net/iivOnCDjcGqGvnwnNWxSbAvb"),
+            thumbnail_info=ThumbnailInfo(
+                mimetype="image/png",
+                width=364,
+                height=153,
+                size=51302,
+            ),
+        ),
+    ),
+    "low": MediaMessageEventContent(
+        msgtype=MessageType.IMAGE,
+        body="putkiteippi.gif",
+        url=ContentURI("mxc://maunium.net/PXbFbVEmcGzkaMXwUOrzZcQM"),
+        info=ImageInfo(
+            mimetype="image/gif",
+            width=640,
+            height=267,
+            size=2352044,
+            thumbnail_url=ContentURI("mxc://maunium.net/QGTVCHOEqUOweASaxKQhEvyD"),
+            thumbnail_info=ThumbnailInfo(
+                mimetype="image/png",
+                width=640,
+                height=267,
+                size=109306,
+            ),
+        ),
+    ),
+    "medium": MediaMessageEventContent(
+        msgtype=MessageType.IMAGE,
+        body="putkiteippi.gif",
+        url=ContentURI("mxc://maunium.net/LNjeTZvDEaUdQAROvWGHLLDi"),
+        info=ImageInfo(
+            mimetype="image/gif",
+            width=1280,
+            height=535,
+            size=7500893,
+            thumbnail_url=ContentURI("mxc://maunium.net/xdhlegZQgGwlMRzBfhNxyEfb"),
+            thumbnail_info=ThumbnailInfo(
+                mimetype="image/png",
+                width=800,
+                height=334,
+                size=417896,
+            ),
+        ),
+    ),
+    "high": MediaMessageEventContent(
+        msgtype=MessageType.IMAGE,
+        body="putkiteippi.gif",
+        url=ContentURI("mxc://maunium.net/HJGpkfVWxZYaBWpOYecOiJtg"),
+        info=ImageInfo(
+            mimetype="image/gif",
+            width=1920,
+            height=802,
+            size=15824736,
+            thumbnail_url=ContentURI("mxc://maunium.net/iIDrkChFzwyZAXycmfZFaAbV"),
+            thumbnail_info=ThumbnailInfo(
+                mimetype="image/png",
+                width=800,
+                height=334,
+                size=362611,
+            ),
+        ),
+    ),
+}
+
 
 class JesariBot(Plugin):
     async def start(self) -> None:
@@ -21,21 +96,4 @@ class JesariBot(Plugin):
 
     @staticmethod
     async def handler(evt: MessageEvent) -> None:
-        await evt.respond(MediaMessageEventContent(
-            msgtype=MessageType.IMAGE,
-            body="putkiteippi.gif",
-            url=ContentURI("mxc://maunium.net/IkSoSYYrtaYJQeCaABSLqKiD"),
-            info=ImageInfo(
-                mimetype="image/gif",
-                height=153,
-                width=364,
-                size=2079294,
-                thumbnail_url=ContentURI("mxc://maunium.net/iivOnCDjcGqGvnwnNWxSbAvb"),
-                thumbnail_info=ThumbnailInfo(
-                    mimetype="image/png",
-                    height=153,
-                    width=364,
-                    size=51302,
-                ),
-            ),
-        ))
+        await evt.respond(quality["high"])
